205de3d82fc7193529f6e2ddbe4c9c6b88e66331
Yanagawa Issa <241205173@ccmailg.meijo-u.ac.jp>
2025-09-02 14:38:37 +0900

    データの保存

--- app/src/main/java/jp/ac/meijou/android/s241205173/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205173/MainActivity.java
@@ -1,8 +1,7 @@
 package jp.ac.meijou.android.s241205173;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,11 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205173.PrefDataStore;
 import jp.ac.meijou.android.s241205173.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,26 +31,15 @@ public class MainActivity extends AppCompatActivity {
         });
         TextView text = findViewById(R.id.text);
         binding.button.setOnClickListener(view -> {
-            var textaf = binding.editTextText.getText().toString();;
+            var textaf = binding.editTextText.getText().toString();
             binding.text.setText(textaf);
         });
-        binding.editTextText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                // テキストが更新される直前に呼ばれる
-            }
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                // 文字を1つ入力された時に呼ばれる
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                // テキストが更新されたあとに呼ばれる
-                binding.text.setText(editable.toString());
-            }
-        });
+    prefDataStore = PrefDataStore.getInstance(this);
+    binding.saveButton.setOnClickListener(view -> {
+        var text2 = binding.editTextText.getText().toString();
+        prefDataStore.setString("name", text2);
+       });
 
     }
 }
