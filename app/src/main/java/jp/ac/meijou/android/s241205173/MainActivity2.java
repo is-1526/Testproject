3ec7d5513890521198685b010e9cc3d778e06b67
Yanagawa Issa <241205173@ccmailg.meijo-u.ac.jp>
2025-09-03 14:38:41 +0900

    アクティビティ2からインテントを複数追加

--- app/src/main/java/jp/ac/meijou/android/s241205173/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205173/MainActivity2.java
@@ -1,6 +1,9 @@
 package jp.ac.meijou.android.s241205173;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205173.databinding.ActivityMain2Binding;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    private ActivityMain2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView text = findViewById(R.id.text);
+        binding.button2.setOnClickListener(view -> {
+            var text2_1 = binding.editTextText2.getText().toString();
+            binding.textView.setText(text2_1);
+        });
+        
+        binding.button5.setOnClickListener(view -> {
+            var intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        binding.button6.setOnClickListener(view -> {
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.yahoo.co.jp "));
+            startActivity(intent);
+        });
+
     }
 }
