2642aaa713f2167212b530c171609778512801f6
Yanagawa Issa <241205173@ccmailg.meijo-u.ac.jp>
2025-09-04 12:19:41 +0900

    WebAPIとの通信で、アスキーアート作成

---
+++ app/src/main/java/jp/ac/meijou/android/s241205173/Gist.java
@@ -0,0 +1,10 @@
+package jp.ac.meijou.android.s241205173;
+
+import java.util.Map;
+
+public class Gist {
+    public Map<String, GistFile> files;
+    public static class GistFile {
+        public String content;
+    }
+}
